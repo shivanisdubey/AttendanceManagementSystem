48f2de1fa5df13f366772de1dd4124da42cd8de0
shivanisdubey <shimana.dubey@gmail.com>
2022-04-07 20:33:58 +0530

    initial

---
+++ Sprint1/Controllers/AttendanceController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sprint1.Models;
+using Sprint1.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace Sprint1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly IAttendanceRepository _attendanceRepository;
+        public AttendanceController(IAttendanceRepository attendanceRepository)
+        {
+            _attendanceRepository = attendanceRepository;
+        } 
+        [HttpGet]
+        [Route("Get by id/{employeeid}")]
+        public List<Attendance> GetAttendance(int EmployeeId)
+        {
+            return _attendanceRepository.GetAttendance(EmployeeId);
+        }
+        [HttpGet]
+        [Route("Get by date/{date}")]
+        public Attendance GetAttendanceByDate(int Id, DateTime date)
+        {
+            return _attendanceRepository.GetAttendanceByDate(Id, date);
+        }
+        [HttpGet]
+        [Route("Get by month/{month}")]
+        public List<Attendance> GetAttendanceByMonth(int EmployeeId, int month, int year)
+        {
+            return _attendanceRepository.GetAttendanceByMonth(EmployeeId, month, year);
+        }
+        [HttpGet]
+        [Route("Get by year/{year}")]
+        public List<Attendance> GetAttendanceByYear(int EmployeeId, int year)
+        {
+            return _attendanceRepository.GetAttendanceByYear(EmployeeId, year);
+        }
+        [HttpPost("{update_attendance}")]
+        public void UpdateAttendance(Attendance attendance)
+        {
+            _attendanceRepository.UpdateAttendance(attendance);
+        }
+        [HttpPost]
+        public void AddAttendance(Attendance attendance)
+        {
+            _attendanceRepository.AddAttendance(attendance);
+        }
+    }
+}
--- Sprint1/Repositories/IAttendanceRepository.cs
+++ Sprint1/Repositories/IAttendanceRepository.cs
@@ -7,7 +7,8 @@ namespace Sprint1.Repositories
     public interface IAttendanceRepository
     {
         void AddAttendance(Attendance attendance);
-        void UpdateAttendance(int AttendanceId);
+        void UpdateAttendance(Attendance attendance);
+        void DeleteAttendance(Attendance attendance);
         List<Attendance> GetAttendanceByMonth(int EmployeeId,int month,int year);
         List<Attendance> GetAttendanceByYear(int EmployeeId, int year);
         Attendance GetAttendanceByDate(int EmployeeId,DateTime date);
